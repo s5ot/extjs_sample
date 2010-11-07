aa289ef1a537d57e4b85a3af5e8eedb0917f709e
sogo <sogo.ohta@gmail.com>
2010-11-08 02:57:23 +0900

    ExtJSのサンプル

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExtjsSample::Application.config.secret_token = '50d64155b7b15bc3d032df744e58abfa8262a94b657566e1afda3c6a5a07606954a0b9da8b648fdf4a01c56ff3ce1eef27f36d2c2a974c38a0e69909e65513e4'
